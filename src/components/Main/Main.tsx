5641dacc2c7ebce43f9232a1b6cdd8b2c5c7622b
Pavlonto <pavelpiakarski@yandex.by>
2023-03-20 15:28:45 +0300

    main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Main.module.css"
 
 
-function Main () {
+function Main() {
+
+    const [button, setButton] = useState(false)
+
+    const onClickHandler = () => {
+        setButton(!button)
+    }
+
     return (
         <div className={s.main}>
+            <section className={s.image}>
+                <div className={button ? s.blur : ""}>
+                    {
+                        button &&
+                        <div>
+                            <div className={s.blur_off} onClick={onClickHandler}>x</div>
+                            <div className={s.blur_text}>
+                                <h2 className={s.text_title}>
+                                    Вас приветствует место красоты "Money Cure"
+                                </h2>
+                                <p className={s.text_p}>
+
+                                </p>
+                            </div>
+                        </div>
+                    }
+                </div>
+            </section>
+            <div className={s.button} onClick={onClickHandler}>
+                {!button && <button className={s.btn}>
+                    Узнать больше
+                </button>}
+            </div>
         </div>
     )
 }
